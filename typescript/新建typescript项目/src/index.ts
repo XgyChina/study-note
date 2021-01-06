39ab0d12cbc50a86b14495a0fbaa71e7f02d1221
xuguoyuan <xuguoyuan@gsafety.com>
2021-01-06 11:12:28 +0800

    feat: 增加复制剪切功能

--- typescript/新建typescript项目/src/index.ts
+++ typescript/新建typescript项目/src/index.ts
@@ -1,30 +1,14 @@
-import fs from 'fs';
-import path from 'path';
 
-/**
- * 传入一个文件路径，遍历路径下的文件和文件夹
- * @param {*} mypath
- */
-const findFiles = (mypath: string) => {
-    const files = fs.readdirSync(mypath); // 同步写法
-    files.forEach((filename: string) => {
-        // 获取当前文件的绝对路径
-        const filedir = path.join(mypath, filename);
-        findPath(filedir);
-    });
-};
+import { findMore } from './find-more';
+// import { cutFilesToOnePath } from './cut-files';
+// import { getFilesPath } from './utils/file-utils';
 
-/**
- * 判断路径是文件还是文件夹
- * @param {*} mypath
- */
-const findPath = (mypath: string) => {
-    const stats = fs.statSync(mypath);
-    if (stats.isDirectory()) {
-        findFiles(mypath);
-    } else if (stats.isFile()) {
-        console.log('====文件===', mypath);
-    }
-};
 
-findFiles('C:\\Users\\xuguoyuan\\Desktop\\test');
+const start = () => {
+    // const files = getFilesPath('E:\\迅雷下载\\字幕\\15500+字幕');
+    // console.log('----files----', files);
+    findMore('E:\\迅雷下载\\字幕\\所有字幕', 'E:\\迅雷下载\\字幕\\11500+字幕', 'E:\\迅雷下载\\字幕\\hebing\\');
+    // cutFilesToOnePath('E:\\迅雷下载\\字幕\\15500+字幕', 'E:\\迅雷下载\\字幕\\所有字幕\\');
+}
+
+start();
